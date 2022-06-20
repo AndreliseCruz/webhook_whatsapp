7d8c82ff04634db25cf4e2b9e831755f94604bae
Andrelise Fernandes Cruz <andrelise@Andrelises-MacBook-Pro.local>
2022-06-20 10:18:31 -0300

    port value changed

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Listening on port ${port}`)
 })
 
 // Accepts POST requests at /webhook endpoint
